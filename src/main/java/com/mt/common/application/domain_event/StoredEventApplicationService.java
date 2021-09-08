c56ee07b1e3f427cfe3335ed5a2251022fa53f6b
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-09-07 21:52:53 -0400

    throw ex when event not found

--- src/main/java/com/mt/common/application/domain_event/StoredEventApplicationService.java
+++ src/main/java/com/mt/common/application/domain_event/StoredEventApplicationService.java
@@ -1,8 +1,8 @@
 package com.mt.common.application.domain_event;
 
 import com.mt.common.domain.CommonDomainRegistry;
-import com.mt.common.domain.model.domain_event.StoredEventQuery;
 import com.mt.common.domain.model.domain_event.StoredEvent;
+import com.mt.common.domain.model.domain_event.StoredEventQuery;
 import com.mt.common.domain.model.restful.SumPagedRep;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +12,15 @@ import java.util.Optional;
 public class StoredEventApplicationService {
     public void retry(long id) {
         Optional<StoredEvent> byId = CommonDomainRegistry.getEventRepository().getById(id);
-        byId.ifPresent(storedEvent -> CommonDomainRegistry.getEventStreamService().next(storedEvent));
+        if (byId.isPresent()) {
+            CommonDomainRegistry.getEventStreamService().next(byId.get());
+        } else {
+            throw new IllegalArgumentException("unable to find stored event with id " + id);
+        }
     }
 
     public SumPagedRep<StoredEvent> query(String queryParam, String pageParam, String skipCount) {
         StoredEventQuery storedEventQuery = new StoredEventQuery(queryParam, pageParam, skipCount);
-        return  CommonDomainRegistry.getEventRepository().query(storedEventQuery);
+        return CommonDomainRegistry.getEventRepository().query(storedEventQuery);
     }
 }
